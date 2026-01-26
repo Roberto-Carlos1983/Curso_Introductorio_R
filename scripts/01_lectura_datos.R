4a067d92a3bcf5ffab9dd86da1683eafedbba001
Roberto-Carlos1983 <rrodriguezperedo@gmail.com>
2026-01-26 14:52:13 -0600

    New script

--- scripts/01_lectura_datos.R
+++ scripts/01_lectura_datos.R
@@ -5,14 +5,14 @@
 
 #Entorno de Desarrollo Integrado (Integrated Development Environment (IDE))
 
-#1. Conociendo el panel de RStudio
+# 1. Conociendo el panel de RStudio
 
 #https://docs.posit.co/ide/user/ide/guide/ui/ui-panes.html
 
-#2. Código en R
+# 2. Código en R
 
 View(mtcars)
-summary(mtcars)
+summary(mtcars,digits=4, quantile.type = 1)
 ?mtcars
 ?summary
 ?View
@@ -24,31 +24,73 @@ View(Titanic)
 
 data_titanic <- data.frame(Titanic)
 sum(data_titanic$Freq)
-aggregate(Freq ~ Sex, data = data_titanic, sum)
+aggregate(Freq ~ Sex+Survived, data = data_titanic, sum)
+?aggregate
+
+iris
+?iris
 
 # =========================================================
-# Título: Sesión 2 - Instalando librerias y cargando información
-# Propósito: Conocer RStudio
+# Título: Sesión 2 - Librerias, instalación de librerias y cargando información
+# Propósito: Conocer herramientas para uso de R
 # =========================================================
 
+#Comprehensive R Archive Network (CRAN)
+#Libreria: conjunto de funciones, conjunto de datos, y código R predeterminado
 
+# 1. Carga de librerias
+#1ra vez: instalar y después llamar a la librería
+#Librería ya instalada: llamar a la librería
 
-
-
-# 1. CARGA DE LIBRERÍAS ----
-# Si no las tienen, deben instalarlas con: install.packages("here")
 install.packages("here")
 library(here)
+install.packages("tidyverse")
 library(tidyverse)
 
-# 2. CONFIGURACIÓN DE RUTAS ----
+ls("package:dplyr")
+
+data_titanic[2,]
+data_titanic |> 
+  slice(2)
+
+data_titanic[10:13,"Class"]
+data_titanic |> 
+  slice(10:13)
+
+# 2. Configuración de rutas para lectura de archivos
+
+# Con setwd, usar doble \\ en cada lugar donde se ingrese a una nueva carpeta
+setwd("")
+
+# Trabajando por proyectos, todo archivo guargado en la misma carpeta del proyecto
+# se cargará de forma directa. Si está dentro de una carpeta, se genera el código 
+# con la identificación de la carpeta
+read.csv("base_datos.csv")
+read.csv("dataset/ultimos/base_datos.csv")
+
 # 'here' detecta automáticamente la raíz del proyecto
 path_datos <- here("data", "raw", "titanic.csv")
 
-# 3. LECTURA DE DATOS ----
-# Usamos read_csv (del paquete tidyverse) porque es más rápido y limpio
-# Asegúrate de haber puesto un archivo CSV en la carpeta data/raw/
-datos <- read_csv(path_datos)
+# 3. Lectura de datos
+
+read.csv()
+
+library(readxl)
+read_xlsx()
+readxl::read_xlsx()
+
+library(foreign)
+read.spss()
+foreign::read.spss()
+
+haven::read_stata()
+
+#Si no se trabaja por proyectos, file.choose() es util para ubicar la ruta en el computador
+#Se ejecuta file.choose() sin ningún argumento
+ 
+file.choose()
+
+datos <- read_csv("copiar ruta")
 
 # 4. EXPLORACIÓN INICIAL ----
 head(datos)    # Ver las primeras 6 filas
